70e1f43a81e8215686b73c7eeb6a7ac76e9716f6
justin.keville@gmail.com <justin.keville@gmail.com@f059e13a-c422-4812-1c7e-072bfa0e084e>
2011-10-21 19:49:29 +0000

    - Changed LevelManager.cs to contain a bool array whose values signify if each level has been completed. - Updated Door.cs in accordance with this. - Deleting the Library directory from the repository so people don't have to keep downloading unnecessary files. I'm pretty sure the Library will update however it needs to once you start playing with the Assets. Let me know if this causes a catastrophe.

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -2,20 +2,25 @@ using UnityEngine;
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
+	public bool[] levelsCompleted;
+	private int numOfLevels;
+	
+	void Awake () {
+		numOfLevels = 2;
+		levelsCompleted = new bool[numOfLevels];
+	}
+	
+	public void checkLevel() {
+		levelsCompleted[Application.loadedLevel] = true;
+	}
+	
+	public bool[] getLevels() {
+		return levelsCompleted;
+	}
+	
+	public int getNumLevels() {
+		return numOfLevels;
+	}
+	
 
-	public int completedLevels = 0;
-	public int redDoors = 0;
-	
-	void Start () {
-	}
-	public void bumpLevels() {
-		completedLevels++;
-	}
-	public int getLevels() {
-		return completedLevels;
-	}
-	public void bumpReds() {
-		redDoors++;
-	}
-	
 } // end class s_LevelManager
