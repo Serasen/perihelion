b0b598cedb2aabd7c2ff732bdf35f8ea88427c36
bassist0403@yahoo.com <bassist0403@yahoo.com@f059e13a-c422-4812-1c7e-072bfa0e084e>
2011-11-08 00:18:58 +0000

    Dialogue.js : displays dialogue in gui window upon trigger PopUpText.js (Changes) : Eliminated unnecessary code, reformatted from Ben's build to current build. testScene.unity (Changes) : Added dialogue and popup text trigger zones, resized death zone, renamed gameObjects for readability my_little_pumpkin.png : Placeholder GUI texture thisissomedialogue.txt : Placeholder dialogue text

--- trunk/Assets/PopUpText.js
+++ trunk/Assets/PopUpText.js
@@ -1,36 +1,30 @@
 var textField: GameObject;
-private var dial : GameObject;
 private var text = false;
 
 function Start()
 {
-	dial = textField;
-	dial.renderer.material.color = Color.red;
+	textField.active = false;
 }
 
 function OnTriggerEnter( coll : Collider)
 {
-	if (coll.gameObject.tag == "Player")
+	if (coll.name == "Player")
 	{
 		Debug.Log("Coll Enter");
-		if(text == false)
-		{
-			dial.gameObject.active = true;
-			text = true;
-		}
+		textField.active = true;
+		textField.renderer.material.color = Color.blue;
 		
 	}
  
 }
 
 
-
 function OnTriggerExit( coll : Collider)
 {
-	if (coll.gameObject.tag == "Player")
+	if (coll.name == "Player")
 	{
 		Debug.Log("Coll Exit");
-		dial.gameObject.active = false;
+		textField.active = false;
 	}
 
 }
